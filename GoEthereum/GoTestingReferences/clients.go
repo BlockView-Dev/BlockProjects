5ec0800f886f0106e4b83a223cd7c7e2e5526e90
Alex Tavarez <76749827+donchicotavarez@users.noreply.github.com>
2021-02-25 15:35:47 -0600

    Add files via upload

---
+++ GoEthereum/GoTestingReferences/clients.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "log"
+		
+		// Ref Sources
+    "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+    client, err := ethclient.Dial("https://mainnet.infura.io")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("we have a connection")
+    _ = client
+}
